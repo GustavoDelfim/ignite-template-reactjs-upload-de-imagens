f2302e6b5a72d3c8e540d2b1365d7a7c3e680cfe
Gustavo Delfim <gusttavodelfim@gmail.com>
2022-08-12 08:37:34 -0300

    feat: fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,28 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+type FeatchImagesResponse = {
+  data: Image[];
+  after: string | null;
+};
+
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = 0 }): Promise<Image[]> {
+    const { data } = await api.post<FeatchImagesResponse>('/images', {
+      params: { pageParam },
+    });
+
+    return data.data || [];
+  }
+
   const {
     data,
     isLoading,
@@ -16,11 +37,12 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+  } = useInfiniteQuery<Image[]>('images', fetchImages, {
+    getNextPageParam: (lastPages, page) => {
+      console.log(lastPages);
+      console.log(page);
+      return null;
+    }},
   );
 
   const formattedData = useMemo(() => {
